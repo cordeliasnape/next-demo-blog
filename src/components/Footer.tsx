0052c15bf69c084f828aad1848eae5b98224906c
Cordelia Snape <snapecordelia@gmail.com>
2023-11-20 15:03:40 +0000

    re add everything?

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-indigo-200  text-white py-4 flex items-center justify-center">
+      <p>Cordelia &copy; 2k23</p>
+    </footer>
+  );
+}
+
+export default Footer;
