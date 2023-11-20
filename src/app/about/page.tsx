ebba6a3e0e476e398760ad17e4ce9887aecb9f60
Cordelia Snape <snapecordelia@gmail.com>
2023-11-20 15:02:03 +0000

    added Tess page, no content

--- src/app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AboutRoute() {
-  return (
-    <section className="bg-indigo-50">
-      <h3>This is my page all about me</h3>
-    </section>
-  );
-}
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-indigo-200  text-white py-4 flex items-center justify-center">
-      <p>Cordelia &copy; 2k23</p>
-    </footer>
-  );
-}
-
-export default Footer;
